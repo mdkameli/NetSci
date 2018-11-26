fa4b87148263e9e28d3a57ecb94106aa59b24241
mdkameli <mdkameli@github.com>
2018-11-26 02:38:12 +0100

    polished

--- Analysis.m
+++ Analysis.m
@@ -2,32 +2,53 @@ close all
 clear all
 clc
 
-
-A = csvread('Adj.csv',1,1); 
+% IMPORT & Polishing Data%
+G = csvread('Adj.csv',1,1);
+N = max(size(G));
+A = sparse(G);
+clear G;
 figure(1)
-spy(A)
+%spy(A)
 
+%% pre-processing
+
+Au = 1*(A+A'>0); % undirected network
+Au = Au - diag(diag(Au)); % clear diagonal (you never know)
+
+% Remove nodes which are NOT connected
+pos = find(sum(Au)~=0);
+A = A(pos,pos);
+Au = Au(pos,pos);
+spy(Au);
 
 %% %%%%%%%%%%%%%%%%% EXTRACT THE DISTRIBUTION %%%%%%%%%%%%%%%%%%%%%%%%%
 
-% distribution
-d = full(sum(A,2)); % in degree, for out degree 2->1
-d = d(d>0); % avoid zero degrees
-k = unique(d); % degree samples
-pk = histc(d,k)'; % counts occurrences
-pk = pk/sum(pk); % normalize to 1
+% Distribution
+N = size(A,1);              % Number of Nodes
+d = full(sum(A));           % Degree Vector
+%d = d(d>0);                 % avoid zero degrees
+Links_num = sum(d);         % Total number of links
+k = unique(d);              % Degree Samples
+pk = histc(d,k)';           % counts occurrences
+pk = pk/sum(pk);            % normalize to 1
 
-% cumulative distribution
+% Cumulative distribution
 Pk = cumsum(pk,'reverse');
 
-% log binning
+% Log Binning
 klog = 10.^(0:0.1:ceil(log10(max(k))));
-pklog = histc(d,klog)'; % counts occurrences
-pklog = pklog/sum(pklog); % normalize to 1
+pklog = histc(d,klog)';     % counts occurrences
+pklog = pklog/sum(pklog);   % normalize to 1
+
+%% %%%%%%%%%%%%%%%%% MOMENTS OF DEGREE DISTRIBUTION %%%%%%%%%%%%%%%%%%%%
+
+Mean_K = mean(k);           % First Moment of prob. distribution
+Var_K = var(k);             % Second Moment of Prob. distribution (Express the spread)
+Skew_K = skewness(k);       % Third Moment of Prob. distribution (How symmetric around average)
 
 
-%% %%%%%%%%%%%%%%%%% SHOW THE RESULTS %%%%%%%%%%%%%%%%%%%%%%%%%
-figure(1)
+%% %%%%%%%%%%%%%%%%%%%%%%%% SHOW THE RESULTS %%%%%%%%%%%%%%%%%%%%%%%%%%% 
+figure(2)
 subplot(2,2,1)
 plot(k,pk,'.')
 grid
@@ -56,15 +77,15 @@ title('logarithmic CCDF plot')
 
 %% %%%%%%%%%%%%%%%%% PURE ML FITTING %%%%%%%%%%%%%%%%%%%%%%%%%
 
-kmin = 50;
-d2 = d(d>=kmin); % restrict range
-ga = 1+1/mean(log(d2/kmin)); % estimate the exponent
+kmin = 40;
+d2 = d(d>=kmin);                 % restrict range
+ga = 1+1/mean(log(d2/kmin));     % estimate the exponent
 disp(['gamma ML = ' num2str(ga)])
 
 
 %% %%%%%%%%%%%%%%%%% SHOW THE RESULTS %%%%%%%%%%%%%%%%%%%%%%%%%
 
-figure(1)
+figure(3)
 subplot(2,2,2)
 hold on
 s1 = k.^(-ga); % build the PDF signal
@@ -86,7 +107,7 @@ axis([xlim min(Pk/2) 2])
 
 
 %% %%%%%%%%%%%%%%%%% ML FITTING WITH SATURATION %%%%%%%%%%%%%%%%%%
-d1=d(d>10)
+d1=d(d>30);
 for ks = 1:max(k)
     kmin = min(d1);
     tmp = mean(log((d1+ks)/(kmin+ks)));
@@ -100,14 +121,14 @@ disp(['gamma ML sat = ' num2str(ga2(ks))])
 
 %% %%%%%%%%%%%%%%%%% SHOW THE RESULTS %%%%%%%%%%%%%%%%%%%%%%%%%
 
-figure(2)
+figure(4)
 semilogy(de)
 grid
-xlabel('k_{sat}')
+xlabel('k_sat')
 ylabel('ML target function')
 title('best k_{sat} value')
 
-figure(3)
+figure(5)
 % data
 loglog(k,Pk,'.')
 hold on
